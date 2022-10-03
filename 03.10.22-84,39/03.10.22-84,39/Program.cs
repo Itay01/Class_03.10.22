b22191b76275c14458d3c8e0a66ff50fce89325d
Itay01 <104745527+Itay01@users.noreply.github.com>
2022-10-03 14:54:45 +0300

    Assignment 39 test

---
+++ 03.10.22-84,39/03.10.22-84,39/Program.cs
@@ -0,0 +1,19 @@
+﻿class Assignment_38
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a symbol:");
+        char symbol = char.Parse(Console.ReadLine());
+        if ((symbol > 'A') && (symbol < 'Z'))
+        {
+            Console.WriteLine("Capital letter");
+        if ((symbol > 'a') && (symbol > 'z'))
+            Console.WriteLine("Small letter");
+        else
+            if ((symbol >= 0) || (symbol <= 0))
+                Console.WriteLine("Digit");
+            else
+                Console.WriteLine("Other symbol");
+        }
+    }
+}
